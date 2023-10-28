89d7cb02aeb9d339515444f223526545e3715a5a
abdfrehat <abood512543@gmail.com>
2023-10-28 18:22:37 +0300

    Dev: Change groupIds and make jasper dependency version dynamically changed

--- services/Selling-Services/fetch/fetch-items-ms/build.gradle.kts
+++ services/Selling-Services/fetch/fetch-items-ms/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("io.spring.dependency-management")
 }
 
-group = "com.sales"
+group = "com.sales.fetch"
 version = "1.0-SNAPSHOT"
 
 
--- services/Selling-Services/reports/receipt-generator-ms/build.gradle.kts
+++ services/Selling-Services/reports/receipt-generator-ms/build.gradle.kts
@@ -4,15 +4,15 @@ plugins {
 	id("io.spring.dependency-management")
 }
 
-group = "com.example"
+group = "com.sales.reports"
 version = "1.0-SNAPSHOT"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("jakarta.validation:jakarta.validation-api:${project.findProperty("jakartaValidationApiVersion")}")
 	implementation("org.projectlombok:lombok:${project.findProperty("lombokVersion")}")
-	implementation("net.sf.jasperreports:jasperreports:6.20.1")
-	implementation("com.github.librepdf:openpdf:1.3.22")
+	implementation("net.sf.jasperreports:jasperreports:${project.findProperty("jasperReportsVersion")}")
+	implementation("com.github.librepdf:openpdf:${project.findProperty("openPdfVersion")}")
 	implementation(project(":models"))
 	annotationProcessor("org.projectlombok:lombok:${project.findProperty("lombokVersion")}")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
