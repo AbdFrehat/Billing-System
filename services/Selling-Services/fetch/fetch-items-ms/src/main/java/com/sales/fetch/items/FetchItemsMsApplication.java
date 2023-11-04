e2688c58bfae7ec069d066659c8ea7191ebbcfb5
abdfrehat <abood512543@gmail.com>
2023-11-04 16:50:29 +0300

    Dev: Finish eureka clients for fetch category.

--- services/Selling-Services/fetch/fetch-items-ms/src/main/java/com/sales/fetch/items/FetchItemsMsApplication.java
+++ services/Selling-Services/fetch/fetch-items-ms/src/main/java/com/sales/fetch/items/FetchItemsMsApplication.java
@@ -3,10 +3,12 @@ package com.sales.fetch.items;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableReactiveMongoRepositories
+@EnableDiscoveryClient
 public class FetchItemsMsApplication {
 
     public static void main(String[] args) {
--- services/Selling-Services/fetch/fetch-stores-ms/src/main/java/com/sales/fetch/stores/FetchStoresMsApplication.java
+++ services/Selling-Services/fetch/fetch-stores-ms/src/main/java/com/sales/fetch/stores/FetchStoresMsApplication.java
@@ -4,10 +4,12 @@ package com.sales.fetch.stores;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableReactiveMongoRepositories
+@EnableDiscoveryClient
 public class FetchStoresMsApplication {
 
     public static void main(String[] args) {
