342cce49292c451d4152048f991e58b4eb84108b
abdfrehat <abood512543@gmail.com>
2023-11-29 23:39:22 +0300

    Dev: Add Jacoco to the project and enforce the code coverage to be 80% on any module to be built successfully.

--- services/Selling-Services/build.gradle.kts
+++ services/Selling-Services/build.gradle.kts
@@ -1,11 +1,14 @@
 plugins {
     id("java")
+    id("jacoco")
 }
 group = "com.selling.parent"
 version = "1.0-SNAPSHOT"
 
 allprojects {
     apply(plugin = "java")
+    apply(plugin = "jacoco")
+
     repositories {
         mavenCentral()
     }
@@ -14,13 +17,51 @@ allprojects {
         targetCompatibility = JavaVersion.VERSION_17
     }
     tasks.test {
-        useJUnit()
+        useJUnitPlatform()
+        finalizedBy(tasks.jacocoTestReport)
     }
+
+    tasks.jacocoTestReport {
+        dependsOn(tasks.test)
+        finalizedBy(tasks.jacocoTestCoverageVerification)
+        reports {
+            html.outputLocation = layout.buildDirectory.dir("$buildDir/reports/coverage/html/")
+        }
+    }
+
+    tasks.jacocoTestCoverageVerification {
+        dependsOn(tasks.jacocoTestReport)
+        violationRules {
+            rule {
+                limit {
+                    minimum = "0.8".toBigDecimal()
+                }
+            }
+        }
+    }
+
+    jacoco {
+        toolVersion = "0.8.11"
+        reportsDirectory = layout.buildDirectory.dir("$buildDir/reports/")
+    }
+
     dependencies {
         implementation("org.projectlombok:lombok:${project.findProperty("lombokVersion")}")
         annotationProcessor("org.projectlombok:lombok:${project.findProperty("lombokVersion")}")
     }
+
+    sourceSets {
+        main {
+            java.srcDirs("src/main/java")
+        }
+        test {
+            java.srcDirs("src/test/java")
+        }
+    }
+
 }
+
+
 
 
 
--- services/Selling-Services/skeleton/src/main/java/com/selling/system/SkeletonApplication.java
+++ services/Selling-Services/skeleton/src/main/java/com/selling/system/SkeletonApplication.java
@@ -1,4 +1,4 @@
-package com.selling.sales;
+package com.selling.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
