4079ca76ec4021bb469651af7715d62ae211ddab
abdfrehat <abood512543@gmail.com>
2023-12-01 12:59:01 +0300

    Dev: change the modules names for data category.

--- services/Selling-Services/settings.gradle.kts
+++ services/Selling-Services/settings.gradle.kts
@@ -43,39 +43,39 @@ findProject(":data:data-manager-ms")?.name = "data-manager-ms"
 
 //get Sub-Category
 include("data:get:data-get-manager-ms")
-include("data:get:data-get-ms")
-include("data:get:data-get-free-ms")
-include("data:get:data-get-opt-ms")
+include("data:get:sales-get-ms")
+include("data:get:sales-get-free-ms")
+include("data:get:sales-get-opt-ms")
 findProject(":data:get:data-get-manager-ms")?.name = "data-get-manager-ms"
-findProject(":data:get:data-get-ms")?.name = "data-get-ms"
-findProject(":data:get:data-get-free-ms")?.name = "data-get-free-ms"
-findProject(":data:get:data-get-opt-ms")?.name = "data-get-opt-ms"
+findProject(":data:get:sales-get-ms")?.name = "sales-get-ms"
+findProject(":data:get:sales-get-free-ms")?.name = "sales-get-free-ms"
+findProject(":data:get:sales-get-opt-ms")?.name = "sales-get-opt-ms"
 
 //save Sub-Category
 include("data:save:data-save-manager-ms")
-include("data:save:data-save-ms")
-include("data:save:data-save-multi-ms")
+include("data:save:sale-save-ms")
+include("data:save:sales-save-ms")
 findProject(":data:save:data-save-manager-ms")?.name = "data-save-manager-ms"
-findProject(":data:save:data-save-ms")?.name = "data-save-ms"
-findProject(":data:save:data-save-multi-ms")?.name = "data-save-multi-ms"
+findProject(":data:save:sale-save-ms")?.name = "sale-save-ms"
+findProject(":data:save:sales-save-ms")?.name = "sales-save-ms"
 
 //delete Sub-Category
 include("data:delete:data-delete-manager-ms")
-include("data:delete:data-delete-ms")
-include("data:delete:data-delete-multi-ms")
-include("data:delete:data-delete-query-ms")
+include("data:delete:sale-delete-ms")
+include("data:delete:sales-delete-ms")
+include("data:delete:sale-delete-query-ms")
 findProject(":data:delete:data-delete-manager-ms")?.name = "data-delete-manager-ms"
-findProject(":data:delete:data-delete-ms")?.name = "data-delete-ms"
-findProject(":data:delete:data-delete-multi-ms")?.name = "data-delete-multi-ms"
-findProject(":data:delete:data-delete-query-ms")?.name = "data-delete-query-ms"
+findProject(":data:delete:sale-delete-ms")?.name = "sale-delete-ms"
+findProject(":data:delete:sales-delete-ms")?.name = "sales-delete-ms"
+findProject(":data:delete:sale-delete-query-ms")?.name = "sale-delete-query-ms"
 
 //update Sub-Category
 include("data:update:data-update-manager-ms")
-include("data:update:data-update-ms")
-include("data:update:data-update-multi-ms")
+include("data:update:sale-update-ms")
+include("data:update:sales-update-ms")
 findProject(":data:update:data-update-manager-ms")?.name = "data-update-manager-ms"
-findProject(":data:update:data-update-ms")?.name = "data-update-ms"
-findProject(":data:update:data-update-multi-ms")?.name = "data-update-multi-ms"
+findProject(":data:update:sale-update-ms")?.name = "sale-update-ms"
+findProject(":data:update:sales-update-ms")?.name = "sales-update-ms"
 
 //Commons
 include("shared-module")
