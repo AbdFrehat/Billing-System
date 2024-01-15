d8a1efe170eba01c0d1c2b6cba7f85b21e3b514c
abdfrehat <abood512543@gmail.com>
2024-01-15 22:18:41 +0300

    start working on securing the system by creating database schema and container

---
+++ scripts/users-system-database/data.sql
@@ -0,0 +1,42 @@
+INSERT INTO authorities (authority_name) VALUES('CREATE_USERS');
+INSERT INTO authorities (authority_name) VALUES('DELETE_USERS');
+INSERT INTO authorities (authority_name) VALUES('UPDATE_USERS');
+INSERT INTO authorities (authority_name) VALUES('GET_USERS');
+INSERT INTO authorities (authority_name) VALUES('CREATE_PROFILES');
+INSERT INTO authorities (authority_name) VALUES('DELETE_PROFILES');
+INSERT INTO authorities (authority_name) VALUES('UPDATE_PROFILES');
+INSERT INTO authorities (authority_name) VALUES('GET_PROFILES');
+INSERT INTO authorities (authority_name) VALUES('CREATE_ORDERS');
+INSERT INTO authorities (authority_name) VALUES('DELETE_ORDERS');
+INSERT INTO authorities (authority_name) VALUES('UPDATE_ORDERS');
+INSERT INTO authorities (authority_name) VALUES('GET_ORDERS');
+INSERT INTO authorities (authority_name) VALUES('GET_FREE_ORDERS');
+INSERT INTO authorities (authority_name) VALUES('GET_OPT_ORDERS');
+INSERT INTO authorities (authority_name) VALUES('EXPORT_ORDERS');
+INSERT INTO authorities (authority_name) VALUES('ARCHIVE_ORDERS');
+INSERT INTO authorities (authority_name) VALUES('RESTORE_ORDERS');
+INSERT INTO authorities (authority_name) VALUES('PRINT_RECEIPT');
+INSERT INTO authorities (authority_name) VALUES('ASSIGN_USERS_PROFILES');
+
+
+INSERT INTO profiles (profile_name) VALUES ('admin');
+
+INSERT INTO profiles_authorities (profile_id, authority_id) VALUES (1, 1);
+INSERT INTO profiles_authorities (profile_id, authority_id) VALUES (1, 2);
+INSERT INTO profiles_authorities (profile_id, authority_id) VALUES (1, 3);
+INSERT INTO profiles_authorities (profile_id, authority_id) VALUES (1, 4);
+INSERT INTO profiles_authorities (profile_id, authority_id) VALUES (1, 5);
+INSERT INTO profiles_authorities (profile_id, authority_id) VALUES (1, 6);
+INSERT INTO profiles_authorities (profile_id, authority_id) VALUES (1, 7);
+INSERT INTO profiles_authorities (profile_id, authority_id) VALUES (1, 8);
+INSERT INTO profiles_authorities (profile_id, authority_id) VALUES (1, 9);
+INSERT INTO profiles_authorities (profile_id, authority_id) VALUES (1, 10);
+INSERT INTO profiles_authorities (profile_id, authority_id) VALUES (1, 11);
+INSERT INTO profiles_authorities (profile_id, authority_id) VALUES (1, 12);
+INSERT INTO profiles_authorities (profile_id, authority_id) VALUES (1, 13);
+INSERT INTO profiles_authorities (profile_id, authority_id) VALUES (1, 14);
+INSERT INTO profiles_authorities (profile_id, authority_id) VALUES (1, 15);
+INSERT INTO profiles_authorities (profile_id, authority_id) VALUES (1, 16);
+INSERT INTO profiles_authorities (profile_id, authority_id) VALUES (1, 17);
+INSERT INTO profiles_authorities (profile_id, authority_id) VALUES (1, 18);
+INSERT INTO profiles_authorities (profile_id, authority_id) VALUES (1, 19);
---
+++ scripts/users-system-database/scripts.sql
@@ -0,0 +1,50 @@
+CREATE SEQUENCE profiles_seq START 1;
+CREATE SEQUENCE authorities_seq START 1;
+
+CREATE TABLE authorities 
+(
+    authority_id INTEGER DEFAULT NEXTVAL('authorities_seq'),
+    authority_name VARCHAR(32) NOT NULL,
+    PRIMARY KEY (authority_id)
+)
+
+CREATE TABLE profiles
+(
+    profile_id INTEGER DEFAULT NEXTVAL('profiles_seq'),
+    profile_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (profile_id),
+	UNIQUE(profile_name)
+)
+
+CREATE TABLE profiles_authorities
+(
+    profile_id INTEGER,
+    authority_id INTEGER,
+    PRIMARY KEY (profile_id, authority_id),
+    FOREIGN KEY (authority_id) REFERENCES authorities(authority_id),
+    FOREIGN KEY (profile_id) REFERENCES profiles(profile_id)
+)
+
+CREATE TABLE users
+(
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    profile_id INTEGER NOT NULL,
+    create_at TIMESTAMP NOT NULL,
+    is_enabled INTEGER NOT NULL,
+    is_account_expired INTEGER NOT NULL,
+    is_credential_expired INTEGER NOT NULL,
+    is_account_locked INTEGER NOT NULL,
+    last_password_changed TIMESTAMP NOT NULL,
+    country VARCHAR(64) NOT NULL,
+    city VARCHAR(64) NOT NULL,
+    street VARCHAR(64) NOT NULL,
+    PRIMARY KEY (username),
+    UNIQUE (email),
+    FOREIGN KEY (profile_id) REFERENCES profiles(profile_id)
+)
+
+
+
