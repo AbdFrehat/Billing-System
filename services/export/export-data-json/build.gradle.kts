899dee155d4b662b399940b3fb76171a7a6fb9ca
abdfrehat <abood512543@gmail.com>
2024-03-30 23:21:58 +0300

    DEV: Do some files structure refactoring.

--- services/export/export-data-json/build.gradle.kts
+++ services/export/export-data-json/build.gradle.kts
@@ -22,11 +22,11 @@ repositories {
 	}
 }
 
+@Suppress("UnstableApiUsage")
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
-	targetCompatibility = JavaVersion.VERSION_17
+	sourceCompatibility = JavaVersion.VERSION_21
+	targetCompatibility = JavaVersion.VERSION_21
 }
-
 tasks.test {
 	useJUnitPlatform()
 	finalizedBy(tasks.jacocoTestReport)
